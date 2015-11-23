73c2571af53a0d09433914623c0589e0dd2331fc
gdsa1022 <heimricusdc@gmail.com>
2015-11-23 12:41:16 +0100

    continuando con mi evaluate_ranking

--- evaluate_ranking.py
+++ evaluate_ranking.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 # Creació de la funció "evaluate_ranking.py". Feta per: Eric Díaz Cívico.
 import numpy as np
-import matplotlib.pyplot as plt
 import os # Carreguem la llibreria corresponent a la funció de introducció de
 # rutes absolutes d'un fitxer
-from sklearn.metrics import average_precision_score
+from sklearn.metrics import label_ranking_average_precision_score
 
 
 ruta_abs = os.path.dirname(os.path.abspath(__file__)) #Obtenim la ruta absoluta de la carpetao n es troben els fitxers
@@ -18,7 +17,7 @@ def Evaluate_Ranking(Direct_llista,train_or_val): #Funció declarada passant com
     Final_file = open(ruta_abs+"\files\Average_precision_"+train_or_val+".txt", "w") #Obrim l'arxiu on escriurem el AP per cada consulta  
     Final_file2 = open(ruta_abs+"\files\Mean_average_precision_"+train_or_val+".txt", "w") ##Obrim l'arxiu on escriurem el MAP per cada consulta  
     
-    #Franc: a estas las creo para que no pete el código, luego se intentará depurar
+    #A continuació crearem els 4 fitxers de sortida corresponents. Més endavant anirem depurant questa part per fer-ho amb els mínims i necessaris
     Final_file_train = open(ruta_abs+"/files/final_file_train.txt" ,'w')
     Final_file2_train = open(ruta_abs+"/files/final_file_train.txt" ,'w')
     Final_file_valid = open(ruta_abs+"/files/final_file_valid.txt" ,'w')
@@ -30,13 +29,13 @@ def Evaluate_Ranking(Direct_llista,train_or_val): #Funció declarada passant com
         final = line.index("\n") #Indicació del final de línea de casa vector de AP's
         if train_or_val == "train":
             fitxer_anot = "Annotation_train.txt"
-            APt = average_precision_score(Direct_llista,fitxer_anot)
+            APt = label_ranking_average_precision_score(Direct_llista,fitxer_anot)
             Final_file.append(APt)
             # A continuació escriurem en el fitxer cada línia de les APS per les imatges d'entrenamen
             Final_file_train.write(line[0:final] + "For Query "+line+":\t" + str(Final_file).replace("\n","").replace("[[","").replace("]]","") + "\n")
         else:
             fitxer_anot2 = "Annotation_valid.txt"
-            APv = average_precision_score(Direct_llista,fitxer_anot2)
+            APv = label_ranking_average_precision_score(Direct_llista,fitxer_anot2)
             Final_file.append(APv)
             # A continuació escriurem en el fitxer cada línia de les APS per les imatges de validació
             Final_file_valid.write(line[0:final] + "For Query "+line+":\t"+ str(Final_file).replace("\n","").replace("[[","").replace("]]","") + "\n") 
@@ -71,4 +70,4 @@ def Evaluate_Ranking(Direct_llista,train_or_val): #Funció declarada passant com
     Final_file2_valid.close() #Tanquem el ftixer per on hem esccrit els valors del lMAP resultants per cada línia
 
 Evaluate_Ranking("features_train.txt","train")
-Evaluate_Ranking("features_valid.txt","valid")
+Evaluate_Ranking("features_valid.txt","val")
